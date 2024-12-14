d4fbebb3bd21cd616e8c4b6178bb515bb75e1c8a
TEIKOU2023 <zhenghengteikou@gmail.com>
2024-12-14 18:20:38 +0900

    Displaying platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,55 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "@/hooks/useGames";
+import { HStack } from "@chakra-ui/react";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  return (
+    <HStack marginY={"10px"}>
+      {platforms.map((platform) => (
+        <PlatformIcon platformId={platform.slug} key={platform.id} />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
+const PlatformIcon = ({ platformId }: { platformId: string }) => {
+  const iconStyle = { opacity: 0.5 }; // 设置图标透明度
+
+  switch (platformId) {
+    case "pc":
+      return <FaWindows style={iconStyle} />;
+    case "playstation":
+      return <FaPlaystation style={iconStyle} />;
+    case "xbox":
+      return <FaXbox style={iconStyle} />;
+    case "nintendo":
+      return <SiNintendo style={iconStyle} />;
+    case "mac":
+      return <FaApple style={iconStyle} />;
+    case "linux":
+      return <FaLinux style={iconStyle} />;
+    case "android":
+      return <FaAndroid style={iconStyle} />;
+    case "ios":
+      return <MdPhoneIphone style={iconStyle} />;
+    case "web":
+      return <BsGlobe style={iconStyle} />;
+    default:
+      return null;
+  }
+};
