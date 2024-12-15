568b4d1fe173dbcface48eaff2f3d34d38ca1993
TEIKOU2023 <zhenghengteikou@gmail.com>
2024-12-15 16:26:18 +0900

    Handle games with no image

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,7 +11,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card.Root>
-      <Image src={getCroppedImageUrl(game.background_image)} />
+      <Image src={getCroppedImageUrl(game.background_image)}  aspectRatio={4 / 3}/>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
 
